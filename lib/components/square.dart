94549931827d5feada07039c95f169f9c7e3cd5d
Rishav Kumar Sinha <sinharishavkumar@gmail.com>
2024-10-20 22:01:08 +0530

    Flutter UI - Chess Pieces Added

--- lib/components/square.dart
+++ lib/components/square.dart
@@ -1,13 +1,24 @@
+import 'package:chess/components/piece.dart';
+import 'package:chess/values/colors.dart';
 import 'package:flutter/material.dart';
 
 class Square extends StatelessWidget {
   final bool isWhite;
-  const Square({super.key, required this.isWhite});
+  final ChessPiece? piece;
+
+  const Square({super.key, required this.isWhite, required this.piece});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: isWhite ? Colors.grey.shade200 : Colors.grey.shade500,
+      color: isWhite ? foregroundColor : backgroundColor,
+      child: piece != null
+          ? Image.asset(
+              piece!.imagePath, // Shows correct pawn image based on isWhite
+              fit: BoxFit.contain,
+              //color: piece!.isWhite ? Colors.white : Colors.black,
+            )
+          : null,
     );
   }
 }
